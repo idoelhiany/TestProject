c8f0f75d5a6d56e31cd9d2b7f53d2379c05e5ad6
idoelhiany <idoelhiany1221@gmail.com>
2024-01-27 12:25:42 +0200

    added more hello world messages

--- TestProject/Test.c
+++ TestProject/Test.c
@@ -3,6 +3,11 @@
 
 int main() {
 	printf("hello world number 2");
+	printf("hello world number 3");
+	printf("hello world number 4");
+	printf("hello world number 5");
+	printf("hello world number 6");
+
 
 	return 0;
 }
